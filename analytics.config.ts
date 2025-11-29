912bdb859633f3b89442b8eeb0af9dd0a81e67d1
Al Aqmar <alaqmarak0810@gmail.com>
2025-11-30 00:21:24 +0530

    config files

--- analytics.config.ts
+++ analytics.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "@prisma/config";
+import "dotenv/config";
 
 export default defineConfig({
   schema: "prisma/analytics.prisma",
+  datasource: {
+    url: process.env.ANALYTICS_DATABASE_URL ?? "",
+  },
 });
